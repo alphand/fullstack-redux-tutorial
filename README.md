e95cb6a885f9bc27258ef2b86a07d627915ba927
Niko Darmawan <darmawan.niko@gmail.com>
2015-11-23 23:40:16 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#My testing ground for FullStack Redux Tutorial
+followed from: http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html
---
+++ test/immutable_spec.js
@@ -0,0 +1,33 @@
+import {expect} from 'chai';
+import {List} from 'immutable';
+
+describe('immutability', () => {
+	describe('a number', () => {
+		function increment(currentState) {
+			return currentState + 1;
+		}
+
+		it('is immutable', () => {
+			let state = 42;
+			let nextState = increment(state);
+
+			expect(nextState).to.equal(43);
+			expect(state).to.equal(42);
+		})
+	});
+
+	describe('A List', () => {
+		function AddMovie(currentState, movie) {
+			return currentState.push(movie);
+		}
+
+		it('is immutabe', () => {
+			let state = List.of('Trainspotting', '28 Days Later');
+			let nextState = AddMovie(state, 'Sunshine');
+
+			expect(nextState).to.equal(List.of('Trainspotting', '28 Days Later', 'Sunshine'));
+			expect(state).to.equal(List.of('Trainspotting', '28 Days Later'));
+
+		})
+	})
+})
